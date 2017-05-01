a0ebaf2a4409273523d22c600b5ab40e53e62424
Theresa Medina <trgmedina@gmail.com>
2017-04-30 23:23:01 -0500

    Adding initial files and folders

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));

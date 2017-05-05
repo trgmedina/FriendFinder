09304c15bb9c703a3fbea8e7a0f2fd84031f1d1d
Theresa Medina <trgmedina@gmail.com>
2017-05-05 00:07:47 -0500

    Working on connecting scripts and updating apiRoutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friends = [
 {
-  "name":"Lindsey Lohan",
-  "photoURL":"https://upload.wikimedia.org/wikipedia/en/7/7d/Lindsay_Lohan_in_strapless_white_gown.jpg",
-  "scores":[
+  name :"Lindsey Lohan",
+  photoURL : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Lindsay_Lohan_%28Headshot%29.jpg/440px-Lindsay_Lohan_%28Headshot%29.jpg",
+  scores :[
       5,
       1,
       4,
@@ -16,35 +16,34 @@ var friends = [
     ]
 },
 {
-  "name":"Patrick Star",
-  "photoURL":"https://upload.wikimedia.org/wikipedia/en/7/7e/Patrick_Star.png",
-  "scores":[
+  name :"Patrick Star",
+  photoURL :"https://upload.wikimedia.org/wikipedia/en/7/7e/Patrick_Star.png",
+  scores :[
+      3,
       5,
-      1,
-      4,
-      4,
-      5,
-      1,
       2,
-      5,
       4,
-      1
+      2,
+      1,
+      5,
+      5,
+      3,
+      3
     ]
 },
 {
-  "name":"Kanye West",
-  "photoURL":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg/220px-Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
+  name :"Kanye West",
+  photoURL :"https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg/440px-Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg",
       2,
+      3,
+      2,
+      1,
       5,
-      4,
-      1
+      1,
+      3,
+      2,
+      1,
+      5
     ]
 },
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,34 @@ module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
 		res.json(friends);
 	});
+
+	app.post("/api/friends", function(req, res) {
+		var newFriend = req.body;
+		var newFriendScores = newFriend.responses;
+		var totalDifference = 0;
+		var bestMatch = 100;
+		var friendsIndex = 0;
+
+		console.log(newFriend.responses);
+
+		for (var i = 0; i< friends.length; i++) {
+			totalDifference = 0; 
+
+			for (var x = 0; x < newFriendScores.length; x++) {
+				var difference = Math.abs(newFriendScores[x] - friends[i].scores[x]);
+				totalDifference += difference;
+			}
+
+			if (totalDifference < bestMatch) {
+				bestMatch = totalDifference;
+				friendsIndex = i;
+			}
+		}
+
+		console.log('Best Match', friends[friendsIndex]);
+
+		friends.push(newFriend);
+		res.json(friends[friendsIndex]);
+	});
+
 };
